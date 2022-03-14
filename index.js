354c6df7b9219b893577ae55d91bd26e59738fc2
Christopher X Gerber <christopher.x.gerber@gmail.com>
2022-03-13 21:51:45 -0700

    throttle ws sends

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'))
 
 io.on('connection', (socket) => {
   console.log('client connected', socket.id);
   socket.on('pointer', coords => {
-    console.log('sending coords from ', socket.id)
+    // console.log('sending coords from ', socket.id)
     io.emit('pointer', { id: socket.id, coords: coords })
   })
   socket.on('disconnect', () => {
